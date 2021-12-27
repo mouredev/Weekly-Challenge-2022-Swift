c503dab0f1eeb4015e6887ff92571e7ecec8c1f5
Pablo-M-L <pmlopez@gmail.com>
2021-12-27 22:47:28 +0100

    reto 0 mouredev semana del 27 de diciembre del 2021

--- WeeklyChallenge2022.playground/Pages/Challenge0.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge0.xcplaygroundpage/Contents.swift
@@ -6,4 +6,17 @@
  * Aquí aparecerá el enunciado y podrás resolverlo en el propio fichero.
  */
 
-//: [Next](@next)
+for i in 1...100{
+    var texto = ""
+    
+    if i % 3 == 0{
+        texto += "fizz"
+    }
+    if i % 5 == 0{
+        texto += "buzz"
+    }
+    if texto == ""{
+        texto = String(i)
+    }
+    print("\(texto) \n")
+}
