47e2a72122776809b89c041a0b5cd939d30e8b80
Jaime Fernández <dev@jaimefere.es>
2021-12-29 14:02:15 +0100

    Jaimefere's solution (7 s)

--- WeeklyChallenge2022.playground/Pages/Challenge0.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge0.xcplaygroundpage/Contents.swift
@@ -19,3 +19,14 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+//let startTime = NSDate()
+(1...100).forEach { num in
+    if(num % 5 == 0) {
+        print("\(num % 3 == 0 ? "fizz" : "")buzz")
+    } else if(num % 3 == 0) {
+        print("fizz")
+    } else {
+        print(num)
+    }
+}
+//print("execution time: \(NSDate().timeIntervalSince(startTime as Date) * 1000) ms")
