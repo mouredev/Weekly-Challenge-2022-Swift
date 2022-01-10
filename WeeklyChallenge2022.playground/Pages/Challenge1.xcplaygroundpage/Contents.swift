d265b1aeb2c7728c0c55fd4131716977e96758e3
Brais Moure <braismoure@mouredev.com>
2022-01-10 22:02:00 +0100

    Resolución Reto #1 y enunciado Reto #2

--- WeeklyChallenge2022.playground/Pages/Challenge1.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge1.xcplaygroundpage/Contents.swift
@@ -13,9 +13,15 @@ import Foundation
  * Dos palabras exactamente iguales no son anagrama.
  *
  * Información adicional:
- * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🗓reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
  * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
  * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+func isAnagram(wordOne: String, wordTwo: String) -> Bool {
+    return wordOne.lowercased() == wordTwo.lowercased() ? false : wordOne.lowercased().sorted().elementsEqual(wordTwo.lowercased().sorted())
+}
+
+print(isAnagram(wordOne: "amor", wordTwo: "roma"))
