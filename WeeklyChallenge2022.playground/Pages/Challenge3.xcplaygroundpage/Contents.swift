8c8679456a3fb065027b9fa756b10957f456fbb0
smontoy <smontoy@gmail.com>
2022-01-18 20:41:20 +0100

    Solución Reto03

--- WeeklyChallenge2022.playground/Pages/Challenge3.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge3.xcplaygroundpage/Contents.swift
@@ -24,7 +24,7 @@ import Foundation
 var numPrimos: [Int] = []
 var num = 2
 
-while (num < 200)
+while (num < 100)
 {
     if (((num % 2) != 0) || (num == 2))  && (((num % 3) != 0) || (num == 3)) && (((num % 5) != 0) || (num == 5)) && (((num % 7) != 0) || (num == 7))
      {
