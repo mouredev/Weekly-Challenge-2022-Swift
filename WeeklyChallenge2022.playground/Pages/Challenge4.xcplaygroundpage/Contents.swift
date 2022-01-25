afc30e7989e9071f7b84fedfd5a64290ca240c49
Diego Domínguez Herreros <ddominhe@emeal.nttdata.com>
2022-01-25 12:31:08 +0100

    with class and enum

--- WeeklyChallenge2022.playground/Pages/Challenge4.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge4.xcplaygroundpage/Contents.swift
@@ -19,3 +19,60 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+class Polygon {
+    private var side: Double
+    private var height: Double
+    internal init(side: Double, height: Double){
+        self.side = side
+        self.height = height
+    }
+    func getArea() -> Double {
+        return self.side * self.height
+    }
+}
+
+class Triangle: Polygon {
+    override init(side: Double, height: Double){
+        super.init(side: side, height: height)
+    }
+    
+    override func getArea() -> Double {
+        return super.getArea() / 2.0
+    }
+}
+
+class Square: Polygon {
+    init(side: Double){
+        super.init(side: side, height: side)
+    }
+}
+
+class Rectangle: Polygon {
+    override init(side: Double, height: Double){
+        super.init(side: side, height: height)
+    }
+}
+
+enum PolygonType {
+    case TriangleType(side: Double, height: Double)
+    case SquareType(side: Double)
+    case RectangeType(side: Double, height: Double)
+    func getArea() -> Double {
+        switch(self) {
+        case .TriangleType(side: let side, height: let height):
+            return side * height / 2.0
+        case .SquareType(side: let side):
+            return side * side
+        case .RectangeType(side: let side, height: let height):
+            return side * height
+        }
+    }
+}
+
+print(Triangle(side: 10, height: 10).getArea())
+print(Square(side: 10).getArea())
+print(Rectangle(side: 5, height: 10).getArea())
+print(PolygonType.TriangleType(side: 10, height: 10).getArea())
+print(PolygonType.SquareType(side: 10).getArea())
+print(PolygonType.RectangeType(side: 5, height: 10).getArea())
