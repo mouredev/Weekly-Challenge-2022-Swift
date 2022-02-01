a6045dd63615f86aef6e980bd76d803937c816ee
Jdevelop057 <jdevelop057@gmail.com>
2022-02-01 17:44:31 +0100

    Weekly Challenge 4

--- WeeklyChallenge2022.playground/Pages/Challenge4.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge4.xcplaygroundpage/Contents.swift
@@ -19,3 +19,28 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+enum Polygon{
+    case SQUARE(side: Double)
+    case RECTANGLE(side: Double, side2: Double)
+    case TRIANGLE(side: Double, side2: Double)
+    func getArea() -> Double{
+        switch(self){
+            case .SQUARE(let side):
+                return side*side
+            case .RECTANGLE(let side, let side2):
+                return side*side2
+            case .TRIANGLE(let side, let side2):
+                return (side*side2) / 2
+    }
+
+}}  
+
+
+func main(){
+    print(Polygon.SQUARE(side: 6).getArea())
+    print(Polygon.RECTANGLE(side: 7.5, side2: 12.20).getArea())
+    print(Polygon.TRIANGLE(side: 4.18, side2: 6).getArea())
+}
+
+main()
