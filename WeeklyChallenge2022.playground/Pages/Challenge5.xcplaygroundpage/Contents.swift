0f77944461aa40686517240ffd7054aa47bdf1f4
Fedex <fedex6@gmail.com>
2022-02-02 00:17:22 -0300

    Solucion Reto #5 : PHP
    Esta vez si me acote en las locuras

--- WeeklyChallenge2022.playground/Pages/Challenge5.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge5.xcplaygroundpage/Contents.swift
@@ -18,3 +18,38 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+
+// PHP
+//
+// Se puede probar la solucion en https://poio.com.ar/retos_moure/5/
+
+<?php
+    function calcularAspectRatio($image){
+      // Tomar los valores de la imagen
+      $h  = getimagesize($image)[0]; // Alto
+      $w  = getimagesize($image)[1]; // Ancho
+
+      $cos = round( ($h / $w) , 3);  // Dividimos
+
+      for($multi = 1; $multi <= $h; $multi++){ // Vamos a chequear todos los numero hasta el Alto, el primero que aparezca es el Aspect Ratio
+        $resultado = round(($cos * $multi),2);
+        $resultado_int = intval($cos * $multi);
+
+        // existe una funcion is_int() para saber si es un numero entero
+        // pero por algun motivo me daba siempre falso, asi que meto este
+        // parche para chequear entre uno y otro si son iguales.
+        // intval() solo devuelve el valor entero, por lo que si es igual
+        // al resultado que no esta "formateado", es porque ya daba numero
+        // entero.
+
+        if($resultado == $resultado_int){ 
+          return "[+] Imagen Analizada: ".$image."<br />
+                  [+] Dimensiones: ".$h." x ".$w."<br />
+                  [+] Aspect Ratio => ".$resultado.":".$multi;
+        }
+      }
+    }
+
+    echo calcularAspectRatio('https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png');
+  ?>
