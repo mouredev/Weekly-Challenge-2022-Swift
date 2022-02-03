567d8d4932c9908a8027740edcf9b579c8cbd15d
Diego Domínguez Herreros <ddominhe@emeal.nttdata.com>
2022-02-03 06:58:37 +0100

    s

--- WeeklyChallenge2022.playground/Pages/Challenge5.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge5.xcplaygroundpage/Contents.swift
@@ -53,7 +53,7 @@ class Image {
     init(url urlValue: String) {
         if let url = URL(string: urlValue) {
             guard let data = try? Data(contentsOf: url) else {
-                fatalError()
+                return
             }
             if let image = UIImage(data: data) {
                 self.width = Int(image.size.width)
@@ -63,10 +63,65 @@ class Image {
             }
         }
     }
+    init(asynchronous urlValue: String) {
+        if let url = URL(string: urlValue) {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else {
+                    return
+                }
+                self.width = Int(image.size.width)
+                self.height = Int(image.size.height)
+                let factor = self.width.greatestCommonFactor(value: self.height)
+                self.aspectRatio = "\(self.width/factor):\(self.height/factor)"
+            }.resume()
+        }
+    }
+    init(synchronous urlValue: String) {
+        if let url = URL(string: urlValue) {
+            let semaphore = DispatchSemaphore(value: 0)
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else {
+                    semaphore.signal()
+                    return
+                }
+                self.width = Int(image.size.width)
+                self.height = Int(image.size.height)
+                let factor = self.width.greatestCommonFactor(value: self.height)
+                self.aspectRatio = "\(self.width/factor):\(self.height/factor)"
+                semaphore.signal()
+            }.resume()
+            semaphore.wait(timeout: .distantFuture)
+        }
+    }
 }
 
 let image = Image(url: "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png")
+print("------------------------")
 print(image.width)
 print(image.height)
 print(image.aspectRatio)
+print("------------------------")
+let image1 = Image(synchronous: "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png")
+print(image1.width)
+print(image1.height)
+print(image1.aspectRatio)
+print("------------------------")
+let image2 = Image(asynchronous: "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png")
+print(image2.width)
+print(image2.height)
+print(image2.aspectRatio)
+print("------------------------")
+sleep(3)
+print(image2.width)
+print(image2.height)
+print(image2.aspectRatio)
+print("------------------------")
 //128:41
