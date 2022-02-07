1cd348cece4c0b78c4750dd50e7a8c76db116708
Jaime Fernández <dev@jaimefere.es>
2022-02-07 15:20:26 +0100

    #5 Jaimefere's solution

--- WeeklyChallenge2022.playground/Pages/Challenge5.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge5.xcplaygroundpage/Contents.swift
@@ -18,3 +18,32 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+import ImageIO
+
+func showAspectRatioOf(imageLink: String) {
+    let imageURL = URL(string: imageLink)!
+    let imageName = imageLink.split(separator: "/").last!.split(separator: ".").first!
+    let source = CGImageSourceCreateWithURL(imageURL as CFURL, nil)
+    let imageHeader = CGImageSourceCopyPropertiesAtIndex(source!, 0, nil)! as NSDictionary
+    if let imageWidth = imageHeader["PixelWidth"] as? Int,
+       let imageHeight = imageHeader["PixelHeight"] as? Int {
+        var minWidth = imageWidth
+        var minHeight = imageHeight
+        (1...min(imageWidth, imageHeight)).forEach { i in
+            if((imageWidth % i == 0) && (imageHeight % i == 0)) {
+                minWidth = imageWidth / i
+                minHeight = imageHeight / i
+            }
+        }
+        print("\(String(describing: imageName)) tiene \(minWidth):\(minHeight) de ratio")
+    } else {
+        print("No se ha podido calcular el ratio de \(String(describing: imageName))")
+    }
+}
+
+showAspectRatioOf(imageLink: "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png")
+showAspectRatioOf(imageLink: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Aspect_ratio_16_9_example.jpg")
+showAspectRatioOf(imageLink: "https://upload.wikimedia.org/wikipedia/commons/4/43/Aspect_ratio_4_3_example.jpg")
+showAspectRatioOf(imageLink: "https://pbs.twimg.com/profile_images/918389307831934976/22ktBpnu_400x400.jpg")
+
