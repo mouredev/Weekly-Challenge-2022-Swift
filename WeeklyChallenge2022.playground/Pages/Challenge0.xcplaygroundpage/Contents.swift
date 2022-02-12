03cb4c3c790527af7985908bcc3cda8963c2fa66
Pablo Aller <pablusprogramador@gmail.com>
2022-02-12 15:19:46 +0100

    comienzo del reto

--- WeeklyChallenge2022.playground/Pages/Challenge0.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge0.xcplaygroundpage/Contents.swift
@@ -20,8 +20,4 @@ import Foundation
  *
  */
 
-for index in 1...100 {
-    let divisibleByThree = index % 3 == 0
-    let divisibleByFive = index % 5 == 0
-    print("\((divisibleByThree && divisibleByFive) ? "fizzbuzz" : (divisibleByThree ? "fizz" : (divisibleByFive ? "buzz" : index.description)))")
-}
+
