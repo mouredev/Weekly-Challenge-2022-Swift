a92b948c8c0e26e784731dd168eeac6a2840bba2
Juan Antonio Lagos <lagoscarrera@gmail.com>
2022-02-15 16:17:02 +0100

    Solucion pampincho

--- WeeklyChallenge2022.playground/Pages/Challenge7.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge7.xcplaygroundpage/Contents.swift
@@ -19,3 +19,74 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+func WordCounter(sentence: String){
+    var Dict: [String:Int] = [String:Int]()
+
+    let TempArray: [String] = PhraseSplitter(sentence: PhraseCleaner(sentence: sentence))
+
+    for item in TempArray {
+        if (Dict[item] == nil) {
+            Dict[item] = 1
+        } else {
+            Dict[item]! += 1
+        }
+    }
+    
+    print("Sentence word count: \(sentence)")
+    for(key, value) in Dict{
+        print(key, value, separator: "...")
+    }
+    print()
+}
+
+// Limpia la frase de los signos de puntuación y pasa todas las letras a minusculas
+func PhraseCleaner(sentence: String) -> String {
+    let ForbiddenCharacters: [String] = [".", ",", ";", ":", "\u{0022}", "\u{0027}", "(", ")", "!", "¡", "?", "¿", "...", "-", "_"];
+    
+    var stringTemp = ""
+    
+    for char: Character in sentence {
+        if(!ForbiddenCharacters.contains(String(char))) {
+            stringTemp += String(char)
+        }
+    }
+    
+    return stringTemp.lowercased()
+}
+
+// Separa el string en palabras
+func PhraseSplitter(sentence: String) -> [String] {
+    let spacer = " "
+    var ArrayTemp: [String] = [String]()
+    var stringTemp: String = ""
+    
+    for char: Character in sentence {
+        if (String(char) != spacer){
+            stringTemp += String(char)
+        } else if(stringTemp != "") {
+            ArrayTemp.append(stringTemp)
+            stringTemp = ""
+        }
+    }
+    
+    if (stringTemp != "") {
+        ArrayTemp.append(stringTemp)
+        stringTemp = ""
+    }
+    
+    return ArrayTemp
+}
+
+let Temp: [String] = [
+    "Hello world!",
+    "Hola hackermens",
+    "pampincho, Pampincho; PAMPINCHO- pamPinCho. Parece que FUNCIONA!",
+    "tris, tras, trum, tris, TRUS, TRIS, TRIM",
+    "Gracias, MIL GRACIAS: por ver mi solución. GRACIAS COMPAÑEROS"
+]
+
+for sentence in Temp {
+    WordCounter(sentence: sentence)
+}
+
