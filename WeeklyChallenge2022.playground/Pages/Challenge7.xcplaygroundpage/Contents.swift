d21e477d967e8143f80317f1167179df50c342f0
Dani <alegandro2507@gmail.com>
2022-02-16 19:57:21 +0100

    Solución Reto #7

--- WeeklyChallenge2022.playground/Pages/Challenge7.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge7.xcplaygroundpage/Contents.swift
@@ -19,3 +19,27 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+print(counter(text: "Hola mundo"))
+print(counter(text: textTest()))
+
+func counter(text: String) -> String {
+    let textAux = text.trimmingCharacters(in: .punctuationCharacters)
+        .lowercased()
+        .split(separator: " ")
+    
+    var worldDictionary : [String: Int] = [String: Int]()
+    
+    for world in textAux {
+        if worldDictionary[String(world)] != nil {
+            worldDictionary[String(world)] = worldDictionary[String(world)]! + 1
+        }else{
+            worldDictionary[String(world)] = 1
+        }
+    }
+    
+    return "\(worldDictionary)"
+}
+
+func textTest() -> String {
+    return "Swift es un lenguaje de programación multiparadigma creado por Apple enfocado en el desarrollo de aplicaciones para iOS y macOS. Fue presentado en la WWDC 20147​ y está diseñado para integrarse con los Frameworks Cocoa y Cocoa Touch; puede usar cualquier biblioteca programada en Objective-C y llamar a funciones de C. También es posible desarrollar código en Swift compatible con Objective-C bajo ciertas condiciones. Swift tiene la intención de ser un lenguaje seguro, de desarrollo rápido y conciso. Usa el compilador LLVM incluido en Xcode 6. Fue presentado como un lenguaje propietario, pero en el año 2015, con la versión 2.2, pasó a ser de código abierto8​ con la Licencia Apache 2.0."
+}
