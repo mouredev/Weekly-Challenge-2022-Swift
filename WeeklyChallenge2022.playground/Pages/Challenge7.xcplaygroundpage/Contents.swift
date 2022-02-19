5c658f3a1a1a07163847a3c125b9d5e0f8e1540e
Fedex <fedex6@gmail.com>
2022-02-19 10:38:40 -0300

    Solucion Reto #7 : PHP
    Solucion al reto #7 : contar palabras

--- WeeklyChallenge2022.playground/Pages/Challenge7.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge7.xcplaygroundpage/Contents.swift
@@ -14,6 +14,7 @@ import Foundation
  *
  * Información adicional:
  * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
  * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
  * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
@@ -44,3 +45,34 @@ func countWords(text: String) {
 }
 
 countWords(text: "Hola, mi nombre es brais. Mi nombre completo es Brais Moure (MoureDev).")
+// PHP
+//
+// Como siempre, la prueba se puede ver en:
+// https://poio.com.ar/retos_moure/6/
+// donde se puede poner cualquier frase para correr el programa
+
+<?php
+function contarPalabras($string){
+  $resultado = ''; // Iniciar la variable
+  $signos = array('.', ',', ':', '!', '?', '+', '-'); // Array de signos a eliminar del string
+  $string = strtolower($string); // Pasamos todo a minuscula
+  $string = str_replace($signos, '', $string); // Limpiamos de signos la frase
+
+  // Manos a la obra
+  // Contar
+  $separar = explode(' ', $string); // Crea un array con todas las palabras
+  foreach($separar as $palabra){
+    ${$palabra} = ${$palabra} + 1; // Crea una variable con cada palabra, si no existe va a ser uno y si ya existia va a sumar 
+  }
+
+  //Devolver resultado
+  $string_sin_duplicados = array_unique($separar); // Eliminiar palabras duplicadas del array que ya se limpio
+  foreach($string_sin_duplicados as $sin_dupli){
+    $resultado.= '<pre>'.$sin_dupli.': '.${$sin_dupli}.'</pre>'; // Por cada palabra, imprime su resultado
+  }
+
+  return $resultado;
+}
+
+echo contarPalabras('Hola Mundo! ortra vez, en un reto de Moure. Los desafio y los reto a que se suscriban al canal de Moure!');
+?>
