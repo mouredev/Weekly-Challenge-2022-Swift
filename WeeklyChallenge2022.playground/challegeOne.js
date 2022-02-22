37d3e0602021ee82dc19db5345788362c9c38793
System Administrator <root@MacBook-Pro-de-Junior.local>
2022-02-22 20:43:59 -0300

    feat: reto uno completo

--- WeeklyChallenge2022.playground/challegeOne.js
+++ WeeklyChallenge2022.playground/challegeOne.js
@@ -1,5 +1,3 @@
-import Foundation
-
 /*
  * Reto #0
  * EL FAMOSO "FIZZ BUZZ"
@@ -7,7 +5,8 @@ import Foundation
  * Fecha publicación resolución: 03/01/22
  * Dificultad: FÁCIL
  *
- * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), 
+ *  sustituyendo los siguientes:
  * - Múltiplos de 3 por la palabra "fizz".
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
@@ -20,8 +19,9 @@ import Foundation
  *
  */
 
-for index in 1...100 {
-    let divisibleByThree = index % 3 == 0
-    let divisibleByFive = index % 5 == 0
-    print("\((divisibleByThree && divisibleByFive) ? "fizzbuzz" : (divisibleByThree ? "fizz" : (divisibleByFive ? "buzz" : index.description)))")
+for(let i = 1; i< 101; i++){
+    let three = i % 3 == 0;
+    let five = i % 5 == 0;
+
+    console.log(three && five ? "fizzbuzz" : three ? "fizz" : five ? "buzz" : i);
 }
