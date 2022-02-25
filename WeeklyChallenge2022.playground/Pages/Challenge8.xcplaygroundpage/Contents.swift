ebef3931f3bbca43135c4f3a63ffa9d558af22a8
macarronesc <alegandro2507@gmail.com>
2022-02-25 13:00:59 +0100

    Solución Reto #8

--- WeeklyChallenge2022.playground/Pages/Challenge8.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge8.xcplaygroundpage/Contents.swift
@@ -16,3 +16,35 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+var decimal = 102323
+
+print(convert_manual(decimal: decimal))
+print(convert_auto(decimal: decimal))
+print(convert_manual_recursivo(decimal: decimal, binary: 0, count: 1))
+
+func convert_manual(decimal: Int) -> Int {
+    var decimalAux = decimal
+    var binary = 0
+    var count = 1
+
+    while (decimalAux != 0) {
+        binary += ((decimalAux % 2) * count)
+        decimalAux /= 2
+        count *= 10
+    }
+    return binary
+}
+
+func convert_manual_recursivo(decimal: Int, binary: Int, count: Int) -> String {
+    if (decimal != 0){
+        return convert_manual_recursivo(decimal: decimal / 2, binary: binary + ((decimal % 2) * count), count: count * 10)
+    } else {
+        return String(binary)
+    }
+}
+
+func convert_auto(decimal: Int) -> String {
+    return String(decimal, radix: 2)
+}
+
