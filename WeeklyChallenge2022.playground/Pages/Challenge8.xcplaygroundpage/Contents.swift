7fd41d48af8f79b666e18fcb8bc1bf5cfa184e71
Fedex <fedex6@gmail.com>
2022-02-28 11:05:35 -0300

    Reto 8
    Solucion

--- WeeklyChallenge2022.playground/Pages/Challenge8.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge8.xcplaygroundpage/Contents.swift
@@ -11,6 +11,7 @@ import Foundation
  *
  * Información adicional:
  * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
  * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
  * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
@@ -35,3 +36,31 @@ func decimalToBinary(_ decimal: Int) -> String {
 
 print(decimalToBinary(387))
 print(decimalToBinary(0))
+ // PHP
+//
+// Como siempre, la prueba se puede ver en:
+// https://poio.com.ar/retos_moure/8/
+// donde se puede poner cualquier numero
+
+<?php //Funcion
+$binario = '';
+function convertirEnBinario($decimal){
+
+  if($decimal == 0){
+    $binario = '0'.$binario;
+  } else {
+    while($decimal >= 1){
+      $resultado = ($decimal % 2);
+      $binario = $resultado.$binario;
+
+      $decimal = intval($decimal / 2);
+    }
+  }
+
+  return $binario;
+}
+
+echo convertirEnBinario(69420); 
+?>
+
+
