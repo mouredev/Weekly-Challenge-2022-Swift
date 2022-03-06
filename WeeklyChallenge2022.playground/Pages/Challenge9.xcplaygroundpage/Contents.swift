248c08d55dc61c6d7493da3613aefe5fee8d3195
knela81 <97974599+knela81@users.noreply.github.com>
2022-03-06 10:35:06 +0100

    Update Contents.swift

--- WeeklyChallenge2022.playground/Pages/Challenge9.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge9.xcplaygroundpage/Contents.swift
@@ -19,3 +19,94 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+var diccionario = [String: String]()
+
+diccionario["a"] = ".-"
+diccionario["b"] = "-..."
+diccionario["c"] = "-.-."
+diccionario["d"] = "-.."
+diccionario["e"] = "."
+diccionario["f"] = "..-."
+diccionario["g"] = "--."
+diccionario["h"] = "...."
+diccionario["i"] = ".."
+diccionario["j"] = ".---"
+diccionario["k"] = "-.-"
+diccionario["l"] = ".-.."
+diccionario["m"] = "--"
+diccionario["n"] = "-."
+diccionario["o"] = "---"
+diccionario["p"] = ".--."
+diccionario["q"] = "--.-"
+diccionario["r"] = ".-."
+diccionario["s"] = "..."
+diccionario["t"] = "-"
+diccionario["u"] = "..-"
+diccionario["v"] = "...-"
+diccionario["w"] = ".--"
+diccionario["x"] = "-..-"
+diccionario["y"] = "-.--"
+diccionario["z"] = "--.."
+diccionario["1"] = ".----"
+diccionario["2"] = "..---"
+diccionario["3"] = "...--"
+diccionario["4"] = "....-"
+diccionario["5"] = "....."
+diccionario["6"] = "-...."
+diccionario["7"] = "--..."
+diccionario["8"] = "---.."
+diccionario["9"] = "----."
+diccionario["0"] = "-----"
+diccionario["."] = ".-.-.-"
+diccionario[","] = "--..--"
+diccionario["?"] = "..--.."
+diccionario["\""] = ".-..-."
+diccionario["/"] = "-..-."
+diccionario[" "] = "/"
+
+
+func conversorMorse(frase: String) -> String {
+    
+    var traduccion = ""
+    var palabras = frase.split(separator: " ")
+    
+    if frase.contains(where: {(char) in
+        char.isLetter
+    }){
+        print("No es codigo morse")
+        
+        for char in frase {
+            for key in diccionario{
+                if key.key == char.description.lowercased() {
+
+                    traduccion += " \(key.value)"
+
+                }
+            }
+
+
+        }
+        return "\(frase) en morse es: \(traduccion)"
+    }else{
+        print("es codigo morse")
+        for palabra in palabras {
+           
+            for key in diccionario{
+                
+                if key.value == palabra.lowercased() {
+                    
+                    traduccion += key.key
+                    
+                }
+                
+            }
+            
+        }
+        return "\(frase) en texto es: \(traduccion)"
+        
+    }
+    
+}
+print(conversorMorse(frase: "Antonio tiene un coche rojo"))
+print(conversorMorse(frase: ".- -. - --- -. .. --- / - .. . -. . / ..- -. / -.-. --- -.-. .... . / .-. --- .--- ---"))
