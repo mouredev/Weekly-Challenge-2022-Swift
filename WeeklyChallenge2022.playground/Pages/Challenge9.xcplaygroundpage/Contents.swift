2b68362cca924d66bf711cb92c760d0680a9666f
Antonio Benavente del Moral <antonio@benavente-cardador.com>
2022-03-06 18:34:47 +0100

    Reto 9

--- WeeklyChallenge2022.playground/Pages/Challenge9.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge9.xcplaygroundpage/Contents.swift
@@ -19,3 +19,36 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+let frase = "EN UN LUGAR DE LA MANCHA DE CUYO NOMBRE NO QUIERO ACORDARME"
+let latinToMorse = ["A":"._", "B":"_...","C":"_._.","CH":"____", "D":"_..","E":".","F":".._.","G":"__.","H":"....","I":"..","J":".___","K":"_._","L":"._..","M":"__","N":"_.","Ñ":"__.__","O":"___","P":".__.","Q":"__._","R":"._.","S":"...","T":"_","U":".._","V":"..._","W":".__","X":"_.._","Y":"_.__","Z":"__.."]
+let MorseToLatin = ["._":"A", "_...":"B","_._.":"C","____":"CH", "_..":"D",".":"E",".._.":"F","__.":"G","....":"H","..":"I",".___":"J","_._":"K","._..":"L","__":"M","_.":"N","__.__":"Ñ","___":"O",".__.":"P","__._":"Q","._.":"R","...":"S","_":"T",".._":"U","..._":"V",".__":"W","_.._":"X","_.__":"Y","__..":"Z"]
+
+func palabraToMorse(palabra:String,dicionario:[String:String]) -> String {
+ palabra.map { caracter in
+     dicionario[String(caracter)] ?? "?"
+   }.joined(separator: " ")
+  
+}
+
+func fraseToMorse(fase:String, diccionario:[String:String]) -> String {
+   frase.split(separator: " ").map { palabra in
+        palabraToMorse(palabra: String(palabra), dicionario: diccionario)
+    }.joined(separator: "  ")
+}
+
+func palabraLatin(palabra:String,dicionario:[String:String]) -> String {
+    palabra.components(separatedBy: "  ").map { cadena in
+        cadena.split(separator: " ").map { caracter in
+             dicionario[String(caracter)] ?? " "
+         }.joined()
+    }.joined(separator: " ")
+    
+
+  
+}
+
+print(fraseToMorse(fase: frase, diccionario: latinToMorse))
+print(palabraLatin(palabra: fraseToMorse(fase: frase, diccionario: latinToMorse), dicionario: MorseToLatin))
+print(palabraLatin(palabra: "._ _... _._. _.. . .._. __. .... .. .___ _._ ._.. __ _. ___ .__. __._ ._. ... _ .._ .__ _.._ _.__ __..", dicionario: MorseToLatin))
+
