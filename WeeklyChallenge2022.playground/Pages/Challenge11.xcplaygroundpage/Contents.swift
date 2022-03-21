94369e7c5a2a81fe06493d28b0455520980882fa
Jaime Fernández <dev@jaimefere.es>
2022-03-21 15:33:00 +0100

    #11 Jaimefere's solution

--- WeeklyChallenge2022.playground/Pages/Challenge11.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge11.xcplaygroundpage/Contents.swift
@@ -19,3 +19,16 @@ import Foundation
  *
  */
 
+
+func printExclusiveChars(str1: String, str2: String) {
+    print("out1: \(str1.filter{ !str2.contains($0) })")
+    print("out2: \(str2.filter{ !str1.contains($0) })")
+    print()
+}
+
+printExclusiveChars(str1: "Enunciado: Crea una función que reciba dos cadenas como parámetro (str1, str2) e imprima otras dos cadenas como salida (out1, out2).",
+                    str2: "- out1 contendrá todos los caracteres presentes en la str1 pero NO estén presentes en str2.")
+printExclusiveChars(str1: "Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.",
+                    str2: "Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.")
+printExclusiveChars(str1: "- out1 contendrá todos los caracteres presentes en la str1 pero NO estén presentes en str2.",
+                    str2: "- out2 contendrá todos los caracteres presentes en la str2 pero NO estén presentes en str1.")
