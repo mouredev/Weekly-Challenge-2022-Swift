f52492fd8e5225673cbc4f5d6dade137607f3578
Diego Domínguez Herreros <ddominguezh@Air-de-Diego.home>
2022-03-29 22:12:36 +0200

    finish

--- WeeklyChallenge2022.playground/Pages/Challenge13.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge13.xcplaygroundpage/Contents.swift
@@ -16,3 +16,27 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+extension Int {
+    func factorial() -> Int {
+        if self <= 1 {
+            return 1
+        }
+        return self * (self-1).factorial()
+    }
+}
+
+
+print(0.factorial() == 1)
+print(1.factorial() == 1)
+print(2.factorial() == 2)
+print(3.factorial() == 6)
+print(4.factorial() == 24)
+print(5.factorial() == 120)
+print(6.factorial() == 720)
+print(7.factorial() == 5040)
+print(8.factorial() == 40320)
+print(9.factorial() == 362880)
+print(10.factorial() == 3628800)
+print(15.factorial() == 1307674368000)
+print(20.factorial() == 2432902008176640000)
