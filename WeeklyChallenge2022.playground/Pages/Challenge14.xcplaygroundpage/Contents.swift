d6b26be12f1bf27b00537c627a2c84bca5fc33aa
Brais Moure <braismoure@mouredev.com>
2022-04-04 22:51:46 +0200

    Resolución Reto #13 y enunciado Reto #14

--- WeeklyChallenge2022.playground/Pages/Challenge14.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge14.xcplaygroundpage/Contents.swift
@@ -7,7 +7,7 @@ import Foundation
  * Fecha publicación resolución: 11/04/22
  * Dificultad: FÁCIL
  *
- * Enunciado: Escribe una función que calcule si un número dado es un número de Amstrong (o también llamado narcisista).
+ * Enunciado: Escribe una función que calcule si un número dado es un número de Armstrong (o también llamado narcisista).
  * Si no conoces qué es un número de Armstrong, debes buscar información al respecto.
  *
  * Información adicional:
