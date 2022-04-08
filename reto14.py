33137e6d2cac5cb78ba081e2e5bffbd03d64e713
Cris Torres <82010755+CrisLeaf@users.noreply.github.com>
2022-04-07 23:15:05 -0400

    Update reto14.py

--- reto14.py
+++ reto14.py
@@ -1,14 +1,4 @@
-
-
-
+# Funciona solo en Python 3.8 o superior
 
 print("Es un número de Armstrong." if (number := int(input("Ingrese número:"))) == sum([int(n) ** len(str(number)) for n in str(number)]) else "No es un número de Armstrong.")
-
-
-
-
-
-
-
-
 
