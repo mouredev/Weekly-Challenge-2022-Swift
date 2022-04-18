f1bcd25b9a80854f413d3418cfa5d194e77fb7a2
Daniel Argüeso <arg.dani@gmail.com>
2022-04-19 00:37:09 +0200

    Solución propuesta en c# con obtención simple de texto desde internet

---
+++ WeeklyChallenge2022/RandomDataAPI/Entities/LoremIpsumDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace WeeklyChallenge2022.RandomDataAPI.Entities
+{
+    public class LoremIpsumDto
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("uid")]
+        public Guid Uid { get; set; }
+
+        [JsonPropertyName("word")]
+        public string Word { get; set; }
+
+        [JsonPropertyName("words")]
+        public List<string> Words { get; set; }
+
+        [JsonPropertyName("characters")]
+        public string Characters { get; set; }
+
+        [JsonPropertyName("short_sentence")]
+        public string ShortSentence { get; set; }
+
+        [JsonPropertyName("long_sentence")]
+        public string LongSentence { get; set; }
+
+        [JsonPropertyName("very_long_sentence")]
+        public string VeryLongSentence { get; set; }
+
+        [JsonPropertyName("paragraphs")]
+        public List<string> Paragraphs { get; set; }
+
+        [JsonPropertyName("question")]
+        public string Question { get; set; }
+
+        [JsonPropertyName("questions")]
+        public List<string> Questions { get; set; }
+    }
+}
---
+++ WeeklyChallenge2022/RandomDataAPI/RandomDataAPIUrls.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WeeklyChallenge2022.RandomDataAPI
+{
+    internal class RandomDataAPIUrls
+    {
+        private const string BaseUrl = @"https://random-data-api.com/api/";
+
+        internal static Uri LoremIpsum => new (BaseUrl + @"lorem_ipsum/random_lorem_ipsum");
+    }
+}
