27158a6cd64a2a04cd239c82b07c2f582ebfe6de
Lemito66 <lemito66@gmail.com>
2022-06-22 13:02:24 -0500

    Falta resolución reto #10

--- WeeklyChallenge2022.playground/Pages/Challenge10.xcplaygroundpage/Contents.py
+++ WeeklyChallenge2022.playground/Pages/Challenge10.xcplaygroundpage/Contents.py
@@ -1,4 +1,4 @@
-import Foundation
+""" 
 
 /*
  * Reto #10
@@ -19,9 +19,58 @@ import Foundation
  * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
- */
+ */ """
 
-print(isBalanced(expression: "{a + b [c] * (2x2)}}}}"))
+
+def expresion(expresion_a_definir: str):
+    lista = []
+    buscador = ""
+    contador = 0
+    contadorMalvado = 1
+    diccionarioInicioDeExpresion = {"{", "[", "("}
+    diccionarioFinalDeExpresion = {"}", "]", ")"}
+
+    for i in range(0, len(expresion_a_definir)):
+        if expresion_a_definir[i] != "" and expresion_a_definir[i] != " ":
+            if expresion_a_definir[i] in diccionarioInicioDeExpresion:
+                if expresion_a_definir[i] == '{':
+                    buscador = '}'
+                    contador += 1
+                elif expresion_a_definir[i] == '[':
+                    buscador = ']'
+                    contador += 1
+                elif expresion_a_definir[i] == '(':
+                    buscador = ')'
+                    contador += 1
+                for j in range(i+1, len(expresion_a_definir)):
+                    if expresion_a_definir[j] == buscador and j not in lista:
+                        lista.append(j)
+                        contador += 1
+                        break
+            elif expresion_a_definir[i] in diccionarioFinalDeExpresion:
+                if expresion_a_definir[i] == '}':
+                    contadorMalvado += 2
+                elif expresion_a_definir[i] == ']':
+                    contadorMalvado += 2
+                elif expresion_a_definir[i] == ')':
+                    contadorMalvado += 2
+            """ elif i not in lista:
+                contador = 3
+                break """
+    print(contador, contadorMalvado, "\n", len(lista), "\n", lista)
+    if contador % 2 == 0 and contador != 0:
+        return contador
+    else:
+        return contadorMalvado
+
+
+if expresion("{a^4 + (((ax4)}") % 2 == 0:
+    print("Bien")
+else:
+    print("Mal")
+
+
+""" print(isBalanced(expression: "{a + b [c] * (2x2)}}}}"))
 print(isBalanced(expression: "{ [ a * ( c + d ) ] - 5 }"))
 print(isBalanced(expression: "{ a * ( c + d ) ] - 5 }"))
 print(isBalanced(expression: "{a^4 + (((ax4)}"))
@@ -50,3 +99,4 @@ func isBalanced(expression: String) -> Bool {
 
     return stack.isEmpty
 }
+ """
