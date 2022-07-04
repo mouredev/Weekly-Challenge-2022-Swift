8e893244176bf30208bfed866a65e894d63fa49b
joseph <leonus96@gmail.com>
2022-07-04 12:05:59 -0500

    Setup Dart project

---
+++ WeeklyChallenge2022.playground/Pages/Challenge26.xcplaygroundpage/challenge26/bin/challenge26.dart
@@ -0,0 +1,5 @@
+import 'package:challenge26/challenge26.dart' as challenge26;
+
+void main(List<String> arguments) {
+  print('Hello world: ${challenge26.calculate()}!');
+}
---
+++ WeeklyChallenge2022.playground/Pages/Challenge26.xcplaygroundpage/challenge26/test/challenge26_test.dart
@@ -0,0 +1,8 @@
+import 'package:challenge26/challenge26.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
