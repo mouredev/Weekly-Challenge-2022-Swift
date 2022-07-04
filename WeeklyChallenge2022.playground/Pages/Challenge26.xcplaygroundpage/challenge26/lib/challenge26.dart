d6ddf1ffa5bb8d2e77814ca2c848abfe0150bb1b
joseph <leonus96@gmail.com>
2022-07-04 13:19:27 -0500

    Solution Challenge 26 in Dart 🎯
    Solution Challenge 26 with Dart Language.
    
    ### `/lib/challenge26.dart`
    - Draw Square Function.
    - Draw Triangle Function.
    - Draw Diamond Function.
    
    ### `/bin/challenge26.dart`
    - CLI App.
    ## How to run?
    Run `dart bin/challenge26.dart --help`
    
    ### Test
    Run `dart test`.
    Testing file: `test/challenge26_test.dart`

--- WeeklyChallenge2022.playground/Pages/Challenge26.xcplaygroundpage/challenge26/lib/challenge26.dart
+++ WeeklyChallenge2022.playground/Pages/Challenge26.xcplaygroundpage/challenge26/lib/challenge26.dart
@@ -1,3 +1,66 @@
-int calculate() {
-  return 6 * 7;
+String drawTriangle(int sideSize) {
+  var draw = '';
+  for (var i = 0; i < sideSize; i++) {
+    var line = '';
+    for (var j = 0; j <= i; j++) {
+      if (i == sideSize - 1) {
+        line += '*';
+      } else if (j == 0 || j == i) {
+        line += '*';
+      } else {
+        line += ' ';
+      }
+      line += ' ';
+    }
+    line += '\n';
+    draw += line;
+  }
+  return draw;
+}
+
+String drawSquare(int sideSize) {
+  var draw = '';
+  for (var i = 0; i < sideSize; i++) {
+    var line = '';
+    for (var j = 0; j < sideSize; j++) {
+      if (i == 0 || i == sideSize - 1) {
+        line += '*';
+      } else {
+        if (j == 0 || j == sideSize - 1) {
+          line += '*';
+        } else {
+          line += ' ';
+        }
+      }
+      line += ' ';
+    }
+    line += '\n';
+    draw += line;
+  }
+  return draw;
+}
+
+String drawDiamond(int sideSize) {
+  var draw = '';
+  for (var i = 0; i < sideSize - 1; i++) {
+    draw += _drawDiamondLine(sideSize, i);
+  }
+  for (var i = sideSize - 1; i > -1; i--) {
+    draw += _drawDiamondLine(sideSize, i);
+  }
+  return draw;
+}
+
+String _drawDiamondLine(int sideSize, int lineIndex) {
+  var line = '';
+  for (var j = 0; j < sideSize * 2 - 1; j++) {
+    if (j == (sideSize - 1) - lineIndex || j == (sideSize - 1) + lineIndex) {
+      line += '*';
+    } else {
+      line += ' ';
+    }
+    line += ' ';
+  }
+  line += '\n';
+  return line;
 }
--- WeeklyChallenge2022.playground/Pages/Challenge26.xcplaygroundpage/challenge26/test/challenge26_test.dart
+++ WeeklyChallenge2022.playground/Pages/Challenge26.xcplaygroundpage/challenge26/test/challenge26_test.dart
@@ -2,7 +2,38 @@ import 'package:challenge26/challenge26.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('draw square', () {
+    expect(
+      drawSquare(5),
+      '* * * * * \n'
+      '*       * \n'
+      '*       * \n'
+      '*       * \n'
+      '* * * * * \n',
+    );
+  });
+  test('draw triangle', () {
+    expect(
+      drawTriangle(5),
+      '* \n'
+      '* * \n'
+      '*   * \n'
+      '*     * \n'
+      '* * * * * \n',
+    );
+  });
+  test('draw diamond', () {
+    expect(
+      drawDiamond(5),
+      '        *         \n'
+      '      *   *       \n'
+      '    *       *     \n'
+      '  *           *   \n'
+      '*               * \n'
+      '  *           *   \n'
+      '    *       *     \n'
+      '      *   *       \n'
+      '        *         \n',
+    );
   });
 }
