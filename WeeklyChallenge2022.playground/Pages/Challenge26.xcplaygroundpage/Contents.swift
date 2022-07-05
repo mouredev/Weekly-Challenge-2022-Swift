5061e91abda9b3a90c7359b1f230a1aac3fa5f68
Jaime Fernández <dev@jaimefere.es>
2022-07-05 13:30:12 +0200

    Jaimefere's solution #26

--- WeeklyChallenge2022.playground/Pages/Challenge26.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge26.xcplaygroundpage/Contents.swift
@@ -18,3 +18,32 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+
+private enum Figure {
+    case SQUARE
+    case TRIANGLE
+}
+
+private func drawPolygon(side: Int, figure: Figure) {
+    if(side < 2) {
+        print("La figura debe tener al menos 2 de lado\n")
+    } else {
+        (1...side).forEach { row in
+            var rowLine = ""
+            (1...side).forEach { column in
+                if(figure == Figure.SQUARE || column <= row) {
+                    rowLine += "* "
+                }
+            }
+            print(rowLine)
+        }
+        print()
+    }
+}
+
+drawPolygon(side: 1, figure: Figure.SQUARE)
+drawPolygon(side: 2, figure: Figure.SQUARE)
+drawPolygon(side: 2, figure: Figure.TRIANGLE)
+drawPolygon(side: 5, figure: Figure.SQUARE)
+drawPolygon(side: 5, figure: Figure.TRIANGLE)
