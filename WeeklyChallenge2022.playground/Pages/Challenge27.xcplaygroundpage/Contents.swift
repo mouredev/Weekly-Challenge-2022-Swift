4904dc5cf3cf8ee9f73c2e52ba248488e6a89b47
Brais Moure <braismoure@mouredev.com>
2022-07-11 21:10:19 +0200

    Resolución Reto #27 y enunciado Reto #28

--- WeeklyChallenge2022.playground/Pages/Challenge27.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge27.xcplaygroundpage/Contents.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 /*
  * Reto #27
  * VECTORES ORTOGONALES
@@ -16,3 +18,10 @@
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+func areOrthogonal(vectorOne: (first: Int, second: Int), vectorTwo: (first: Int, second: Int)) -> Bool {
+    return vectorOne.first * vectorTwo.first + vectorOne.second * vectorTwo.second == 0
+}
+
+print(areOrthogonal(vectorOne: (first: 1, second: 2), vectorTwo: (first: 2, second: 1)))
+print(areOrthogonal(vectorOne: (first: 2, second: 1), vectorTwo: (first: -1, second: 2)))
