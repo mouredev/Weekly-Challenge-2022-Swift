06e65afdf3ba655c6de8072db188430996d424a2
LuisAlberto22 <80664920+LuisAlberto22@users.noreply.github.com>
2022-07-26 11:40:15 -0500

    Solucion en C#

--- WeeklyChallenge2022.playground/Pages/Challenge29.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge29.xcplaygroundpage/Contents.swift
@@ -19,3 +19,57 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+SortClass<int> sort = new SortClass<int>();
+
+int[] numbers = new int[] { 5, 3, 7 };
+
+sort.Sort(numbers, SortClass<int>.SortDirection.Ascending);
+
+Array.ForEach(numbers, (i) => Console.WriteLine(i));
+
+//La clase generica T debe de implementar la interface IComparable
+public class SortClass<T> where T : IComparable<T>
+{
+	public enum SortDirection
+	{
+		Ascending, Descending
+	}
+
+  //Delegado que retorne un bool
+	private delegate bool Predicate<T1>(T1 first, T1 second);
+  //aqui se guarda las condiciones que se usaran por cada tipo de ordenamiento
+	Dictionary<SortDirection, Predicate<T>> _keys = new Dictionary<SortDirection, Predicate<T>>()
+	{
+		[SortDirection.Ascending] = (first, second) => first.CompareTo(second) > 0,
+		[SortDirection.Descending] = (first, second) => first.CompareTo(second) < 0,
+	};
+  //declaracion de un delegado en el cual guardaremos la condicion a usar
+	Predicate<T> _condition;
+
+	public void Sort(T[] array, SortDirection direction)
+	{
+		_condition = _keys[direction];
+		Sort(array);
+	}
+
+  //algoritmo de burbuja mejorado
+	void Sort(T[] array)
+	{
+		bool changes = true;
+		while (changes)
+		{
+			changes = false;
+			for (int i = 1; i < array.Length; i++)
+			{
+				if (_condition.Invoke(array[i - 1], array[i]))
+				{
+					T aux = array[i - 1];
+					array[i - 1] = array[i];
+					array[i] = aux;
+					changes = true;
+				}
+			}
+		}
+	}
+}
