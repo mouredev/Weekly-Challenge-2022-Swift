edcb7a6d961653fd60a2796057d2cde25fca2bbe
EsAlco <90135660+EsAlco@users.noreply.github.com>
2022-08-01 17:49:55 +0200

    Reto #30

--- WeeklyChallenge2022.playground/Pages/Challenge30.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge30.xcplaygroundpage/Contents.swift
@@ -25,3 +25,39 @@ import Foundation
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+
+func asterisksTextBox(text: String) {
+    // Dividimos en un array de string cada palabra, haciendo la división por espacios
+    let textWordToWord = text.components(separatedBy: " ")
+    
+    // Calculamos el tamaño da cada palabra, las ordenamos de mayor a menor y nos quedamos con la mayor.
+    let longestWord = textWordToWord.map{$0.count}.sorted(by: >)[0] + 4
+    
+    // Creamos la fila de asteriscos inicial y final.
+    var asterisksLine = "*"
+    while asterisksLine.count < longestWord {
+        asterisksLine += "*"
+    }
+    
+    // Dibujamos la primera fila de asteriscos.
+    print(asterisksLine)
+    
+    // Creamos el resto de filas.
+    for word in textWordToWord {
+        var wordText = word
+        // Rellenamos de espacios hasta que mida cada palabra un caracter más que la más larga, incluida ésta.
+        while wordText.count < longestWord - 3 {
+            wordText += " "
+        }
+        // Le añadimos el asterisco final
+        wordText += "*"
+        print("* \(wordText)")
+    }
+    
+    // Dibujamos la última fila de asteriscos.
+    print(asterisksLine)
+}
+
+asterisksTextBox(text: "¡Hola mundo! ¿Cómo estás?")
+
