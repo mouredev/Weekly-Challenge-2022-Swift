9cf62ca7c19dca9e2a518034e0a2d31f7bb93196
Jaime Fernández <dev@jaimefere.es>
2022-08-15 18:21:28 +0200

    Jaimefere's solution #32

--- WeeklyChallenge2022.playground/Pages/Challenge32.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge32.xcplaygroundpage/Contents.swift
@@ -14,3 +14,27 @@ import Foundation
  * - Tienes toda la información sobre los retos semanales en https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+func getTheBigestTwo(numbers: [Int]) -> Int? {
+    if(numbers.count < 2) {
+        print("¡El listado debe contener al menos 2 números!")
+        return nil
+    } else {
+        var biggest = numbers[0]
+        var result = numbers[0]
+        numbers.forEach {
+            if($0 > biggest) {
+                result = biggest
+                biggest = $0
+            }
+        }
+        return result
+    }
+}
+
+print(getTheBigestTwo(numbers: []) ?? "")
+print(getTheBigestTwo(numbers: [4]) ?? "")
+print(getTheBigestTwo(numbers: [4, 4]) ?? "")
+print(getTheBigestTwo(numbers: [4, 6, 1, 8, 2]) ?? "")
+
+
