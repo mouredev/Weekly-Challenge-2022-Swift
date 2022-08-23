2c1af110ed0a18ffca729688dfa515b2ac9372be
Francisco Javier Gallego Lahera <fgallego19@hotmail.com>
2022-08-23 20:06:01 +0200

    Solución Reto #34 Swift

--- WeeklyChallenge2022.playground/Pages/Challenge34.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge34.xcplaygroundpage/Contents.swift
@@ -15,3 +15,97 @@ import Foundation
  * - Tienes toda la información sobre los retos semanales en https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+// CALLS
+
+let arraysTest = [
+    [1, 2, 5],
+    [1, 2, 5, 3],
+    [1, 1, 2],
+    [2, 6, 8],
+    [10, 15, 16]
+]
+
+for arrayTest in arraysTest {
+    do {
+        try getLostNumbers(in: arrayTest)
+    } catch let error as ArrayError {
+        print("\(arrayTest) -> \(error.description)")
+    }
+}
+
+// Data & Calculations
+
+enum ArrayError: Error, CustomStringConvertible {
+    case notSorted(ascending: Bool)
+    case containsRepeatedElements
+    case notEnoughElements
+    
+    var description: String {
+        switch self {
+        case .notSorted(let ascending): return "The array must be sorted in \(ascending ? "ASCENDING" : "DESCENDING") order."
+        case .containsRepeatedElements: return "The array must NOT contain any repeated elements."
+        case .notEnoughElements: return "The array must contain, at least, two elements."
+        }
+    }
+}
+
+func getLostNumbers(in array: [Int]) throws {
+    
+    // Error Handling
+    
+    let ascendingOrder = true
+    
+    let arrayMeetsConditionsResult = array.containsUniqueElementsAndIsSorted(ascending: ascendingOrder)
+    try handleArrayErrors(array: array, withConditionResult: arrayMeetsConditionsResult, wantsAscendingOrder: ascendingOrder)
+    
+    // Calculations
+    
+    var lostNumbers = [Int]()
+    
+    for i in array.first!...array.last! {
+        if array.contains(i) == false {
+            lostNumbers.append(i)
+        }
+    }
+    
+    // Results
+    
+    print("The lost numbers are: \(lostNumbers)")
+}
+
+func handleArrayErrors(array: [Int], withConditionResult conditionResult: Int, wantsAscendingOrder: Bool) throws {
+    guard array.count >= 2 else {
+        throw ArrayError.notEnoughElements
+    }
+    
+    switch conditionResult {
+    case -1: throw ArrayError.notSorted(ascending: wantsAscendingOrder)
+    case 1: throw ArrayError.containsRepeatedElements
+    default: break
+    }
+}
+
+extension Array where Element: Comparable & Hashable {
+    
+    /// Determines whether an array meets any, none or both of the next conditions: its sorted and doesn't contain repeated elements.
+    /// - Returns -1 when: the array is NOT sorted in the given way (ascending or descending)
+    /// - Returns 0 when: the array is sorted and doesn't contain repeated elements.
+    /// - Returns 1 when: the array contains repeated elements.
+    func containsUniqueElementsAndIsSorted(ascending: Bool) -> Int {
+        for (index, item) in self.enumerated() {
+            if index < self.count - 1 {
+                let nextItem = self[index + 1]
+                if item == nextItem {
+                    return 1
+                }
+                
+                if (item > self[index+1] && ascending) || (item < self[index+1] && !ascending) {
+                    return -1
+                }
+            }
+        }
+        
+        return 0
+    }
+}
