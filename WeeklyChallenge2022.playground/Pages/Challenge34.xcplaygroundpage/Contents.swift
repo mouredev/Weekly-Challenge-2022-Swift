4fa933a4751dac6aa9a0f771e6dd9a1eea7bd454
Jaime Fernández <dev@jaimefere.es>
2022-08-28 22:38:27 +0200

    Jaimefere's solution #34

--- WeeklyChallenge2022.playground/Pages/Challenge34.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge34.xcplaygroundpage/Contents.swift
@@ -15,3 +15,51 @@ import Foundation
  * - Tienes toda la información sobre los retos semanales en https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+enum WrongArrayError: Error {
+    case empty
+    case nonUniqueValues
+    case unsorted
+}
+
+func getMissedValues(numbers: Array<Int>) throws -> Array<Int> {
+    var missedValues: Array<Int> = []
+    if(numbers.isEmpty) {
+        throw WrongArrayError.empty
+    }
+    var index = 0
+    try numbers.sorted().forEach { number in
+        if(number != numbers[index]) {
+            throw WrongArrayError.unsorted
+        }
+        index += 1
+    }
+    if(Array(Set(numbers)).count != numbers.count) {
+        throw WrongArrayError.nonUniqueValues
+    }
+    (0...(numbers.last! - numbers.first!)).forEach { index in
+        if(!numbers.contains(index + numbers.first!)) {
+            missedValues.append(index + numbers.first!)
+        }
+    }
+    return missedValues
+}
+
+func printMissedValues(numbers: Array<Int>) {
+    do {
+        print(try getMissedValues(numbers: numbers))
+    } catch WrongArrayError.unsorted {
+        print("El array no es válido porque los elementos no están correctamente ordenados")
+    } catch WrongArrayError.nonUniqueValues {
+        print("El array no es válido porque tiene elementos repetidos")
+    } catch {
+        print("El array está vacío")
+    }
+}
+
+printMissedValues(numbers: [5,5,6,7,8])
+printMissedValues(numbers: [8,7,6,5])
+printMissedValues(numbers: [])
+printMissedValues(numbers: [5])
+printMissedValues(numbers: [5,6,7,8])
+printMissedValues(numbers: [5,8])
