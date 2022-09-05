b080955997fec8f41268fbe736626897133c0cba
Jaime Fernández <dev@jaimefere.es>
2022-09-05 21:45:48 +0200

    Jaimefere's solution #35

--- WeeklyChallenge2022.playground/Pages/Challenge35.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge35.xcplaygroundpage/Contents.swift
@@ -24,3 +24,51 @@ import Foundation
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+enum PokemonType {
+    case electric
+    case fire
+    case plant
+    case water
+    
+    func getEffectivity(opponent: PokemonType) -> Double {
+        switch(self) {
+        case .electric: switch(opponent) {
+            case .electric: return 0.5
+            case .fire: return 1.0
+            case .plant: return 0.5
+            case .water: return 2.0
+            }
+        case .fire: switch(opponent) {
+            case .electric: return 1.0
+            case .fire: return 0.5
+            case .plant: return 2.0
+            case .water: return 0.5
+            }
+        case .plant: switch(opponent) {
+            case .electric: return 1.0
+            case .fire: return 0.5
+            case .plant: return 0.5
+            case .water: return 2.0
+            }
+        case .water: switch(opponent) {
+            case .electric: return 1.0
+            case .fire: return 2.0
+            case .plant: return 0.5
+            case .water: return 0.5
+            }
+        }
+    }
+}
+
+func getAttackDamage(attacker: PokemonType, defender: PokemonType, attack: Int, defence: Int) -> String {
+    if(attack < 1 || attack > 100) {
+        return "El ataque no tiene un valor correcto"
+    } else if(defence < 1 || defence > 100) {
+        return "La defensa no tiene un valor correcto"
+    } else {
+        return "El daño de la pelea es \(50.0 * (Double(attack) / Double(defence)) * attacker.getEffectivity(opponent: defender))"
+    }
+}
+
+print(getAttackDamage(attacker: PokemonType.fire, defender: PokemonType.plant, attack: 30, defence: 60))
