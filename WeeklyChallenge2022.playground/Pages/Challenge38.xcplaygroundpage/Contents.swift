851351038e0721441a908fdbf3754f9066ec5526
Brais Moure <mouredev@gmail.com>
2022-09-27 21:35:34 +0200

    Resolución Reto #38 y enunciado Reto #39

--- WeeklyChallenge2022.playground/Pages/Challenge38.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge38.xcplaygroundpage/Contents.swift
@@ -17,3 +17,40 @@ import Foundation
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+func binaryToDecimal(binary: String) -> Int? {
+
+    let length = binary.count - 1
+    
+    if length < 0 {
+        return nil
+    }
+
+    var decimal: Int? = nil
+
+    for index in 0...length {
+        let digitString = Array(binary)[length - index].description
+        if digitString == "0" || digitString == "1" {
+            if decimal == nil {
+                decimal = 0
+            }
+            decimal! += (Int(digitString) ?? 0) * Int(truncating: pow(2.0, index) as NSDecimalNumber)
+        } else {
+            return nil
+        }
+    }
+
+    return decimal
+}
+
+print(binaryToDecimal(binary: "00110") ?? "Es nulo")
+print(binaryToDecimal(binary: "01100") ?? "Es nulo")
+print(binaryToDecimal(binary: "000000000") ?? "Es nulo")
+print(binaryToDecimal(binary: "00210") ?? "Es nulo")
+print(binaryToDecimal(binary: "001101001110") ?? "Es nulo")
+print(binaryToDecimal(binary: "00b10") ?? "Es nulo")
+print(binaryToDecimal(binary: "") ?? "Es nulo")
+print(binaryToDecimal(binary: "-00110") ?? "Es nulo")
+print(binaryToDecimal(binary: " ") ?? "Es nulo")
+print(binaryToDecimal(binary: " 10011") ?? "Es nulo")
+print(binaryToDecimal(binary: "1O1OO11") ?? "Es nulo")
