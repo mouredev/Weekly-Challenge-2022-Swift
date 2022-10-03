3c194b4f80e3b6ab4fb0f38147ec118aa6b896d8
Brais Moure <mouredev@gmail.com>
2022-10-03 22:01:41 +0200

    Resolución Reto #39 y enunciado Reto #40

--- WeeklyChallenge2022.playground/Pages/Challenge39.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge39.xcplaygroundpage/Contents.swift
@@ -22,3 +22,54 @@ import Foundation
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+// Basado en https://www.genbeta.com/desarrollo/implementando-el-algoritmo-quicksort
+
+func quicksort(array: Array<Int>) -> Array<Int> {
+    return array.isEmpty ? array : quicksort(array: array, first: 0, last: array.count - 1)
+}
+
+func quicksort(array: Array<Int>, first: Int, last: Int) -> Array<Int> {
+
+    var i = first
+    var j = last
+    var array = array
+    let pivot = (array[i] + array[j]) / 2
+
+    while (i < j) {
+
+        while array[i] < pivot {
+            i += 1
+        }
+
+        while array[j] > pivot {
+            j -= 1
+        }
+
+        if i <= j {
+
+            let x = array[j]
+
+            array[j] = array[i]
+            array[i] = x
+
+            i += 1
+            j -= 1
+        }
+    }
+
+    if first < j {
+        array = quicksort(array: array, first: first, last: j)
+    }
+
+    if last > i {
+        array = quicksort(array: array, first: i, last: last)
+    }
+
+    return array
+}
+
+let sortedArray = quicksort(array: [3, 5, 1, 8, 9, 0])
+sortedArray.forEach { element in
+    print(element)
+}
