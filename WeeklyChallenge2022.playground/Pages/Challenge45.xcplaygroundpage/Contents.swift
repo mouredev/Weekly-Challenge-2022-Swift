225d79930b49784cc7355636a1620b644dfe8fc0
Camilo Plaza <camilo.alberto.plaza@gmail.com>
2022-11-12 19:48:45 +0100

    Reto #45

--- WeeklyChallenge2022.playground/Pages/Challenge45.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge45.xcplaygroundpage/Contents.swift
@@ -29,3 +29,53 @@ import Foundation
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+internal func validArray(_ data:  [Int]) -> Bool {
+    let lengthData = data.count
+    if lengthData < 3 { return false }
+    if data.contains(where: { $0  < 0 }) { return false }
+    if data.filter({ $0  == 0 }).count == lengthData { return false }
+    return true
+}
+
+func countGotas(_ data: [Int]) -> Int {
+    guard validArray(data) else {
+        return 0
+    }
+    
+    var countTotal = 0
+    let lengthData = data.count
+    (1..<lengthData-1).forEach { index in
+        let current = data[index]
+        let prev = data[(0..<index)].max() ?? 0
+        let next = data[(index+1..<lengthData)].max() ?? 0
+        let min = [prev, next].min() ?? 0
+        let valueToSum = min - current
+        if valueToSum > 0 {
+            countTotal += valueToSum
+        }
+    }
+    
+    return countTotal
+}
+
+let candidate1 = [2, 1, 2, 3, 0, 4, 0, 4]
+print("En total hay \(countGotas(candidate1)) gotas de agua")
+
+let candidate2 = [2, 3]
+print("En total hay \(countGotas(candidate2)) gotas de agua")
+
+let candidate3 = [2, 3, 2, 3, 5, 1, 5]
+print("En total hay \(countGotas(candidate3)) gotas de agua")
+
+let candidate4 = [2, 7, 2, 3, 0, 1]
+print("En total hay \(countGotas(candidate4)) gotas de agua")
+
+let candidate5 = [2, 0, 2, 3, 0, 0, 0, 4]
+print("En total hay \(countGotas(candidate5)) gotas de agua")
+
+let candidate6 = [2, 0, -2, 3, 0, 0, -1, 4]
+print("En total hay \(countGotas(candidate6)) gotas de agua")
+
+let candidate7 = [0, 0, 0, 0, 0]
+print("En total hay \(countGotas(candidate7)) gotas de agua")
