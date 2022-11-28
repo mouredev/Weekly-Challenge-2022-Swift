f46f2fe5846cae0d20ea5c215c2df0607a237f14
Brais Moure <mouredev@gmail.com>
2022-11-28 21:41:42 +0100

    Resolución Reto #47 y enunciado Reto #48

--- WeeklyChallenge2022.playground/Pages/Challenge47.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge47.xcplaygroundpage/Contents.swift
@@ -8,7 +8,8 @@ import Foundation
  * Dificultad: FÁCIL
  *
  * Enunciado: Crea un función que reciba un texto y retorne la vocal que más veces se repita.
- * Si no hay vocales podrá devolver vacío.
+ * - Ten cuidado con algunos casos especiales.
+ * - Si no hay vocales podrá devolver vacío. 
  *
  * Información adicional:
  * - Usa el canal de nuestro Discord (https://mouredev.com/discord) "🔁reto-semanal"
@@ -17,3 +18,40 @@ import Foundation
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+func mostRepeatedVowel(text: String) -> [String] {
+
+    var vowelCount: [Character : Int] = [:]
+
+    text.lowercased().folding(options: .diacriticInsensitive, locale: .current).forEach { character in
+        if character == "a" || character == "e" || character == "i" || character == "o" || character == "u" {
+            vowelCount[character] = (vowelCount[character] ?? 0) + 1
+        }
+    }
+
+    var mostRepeated: [String] = []
+    var maxRepeated = 0
+    
+    vowelCount.forEach { (vowel: Character, count: Int) in
+        if count >= maxRepeated {
+            if count > maxRepeated {
+                mostRepeated.removeAll()
+            }
+            mostRepeated.append(vowel.description)
+
+            maxRepeated = count
+        }
+    }
+
+    return mostRepeated
+}
+
+print(mostRepeatedVowel(text: "aaaaaeeeeiiioou"))
+print(mostRepeatedVowel(text: "AáaaaEeeeIiiOoU"))
+print(mostRepeatedVowel(text: "eeeeiiioouaaaaa"))
+print(mostRepeatedVowel(text: ".-Aá?aaaBbEeeweIiiOoU:"))
+print(mostRepeatedVowel(text: ".-Aá?aaa BbEeew eIiiOoU:"))
+print(mostRepeatedVowel(text: ".-Aá?aaa BbEeew eEIiiOoU:"))
+print(mostRepeatedVowel(text: ".-Aá?aaa BbEeew eEIiiOoUuuuuu:"))
+print(mostRepeatedVowel(text: "aeiou"))
+print(mostRepeatedVowel(text: "brp qyz"))
