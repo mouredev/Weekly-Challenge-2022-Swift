f0736f5f90e27315969815342f02a5139cacd9c2
Jaime Fernández <dev@jaimefere.es>
2022-12-12 12:20:19 +0100

    ChatGPT's solution #49

--- WeeklyChallenge2022.playground/Pages/Challenge49.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge49.xcplaygroundpage/Contents.swift
@@ -20,3 +20,37 @@ import Foundation
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+func detectHandles(from text: String) -> [String] {
+    let userHandleRegex = try! NSRegularExpression(pattern: "@[a-zA-Z0-9_]+")
+    let hashtagHandleRegex = try! NSRegularExpression(pattern: "#[a-zA-Z0-9_]+")
+    let webHandleRegex = try! NSRegularExpression(pattern: "(www\\.[a-zA-Z0-9_]+|https?://[a-zA-Z0-9_]+)(\\.[a-zA-Z]+)+")
+
+    var handles = [String]()
+
+    // Busca handles de usuario
+    let userHandleMatches = userHandleRegex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+    for match in userHandleMatches {
+        let handle = String(text[Range(match.range, in: text)!])
+        handles.append(handle)
+    }
+
+    // Busca handles de hashtags
+    let hashtagHandleMatches = hashtagHandleRegex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+    for match in hashtagHandleMatches {
+        let handle = String(text[Range(match.range, in: text)!])
+        handles.append(handle)
+    }
+
+    // Busca handles de web
+    let webHandleMatches = webHandleRegex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+    for match in webHandleMatches {
+        let handle = String(text[Range(match.range, in: text)!])
+        handles.append(handle)
+    }
+
+    return handles
+}
+
+print(detectHandles(from: "Sorteo #aDEViento2022 - Sábado 10 de diciembre (10/24) - (x2) Libros 'Inteligencia matemática/Apocalipsis matemático' de Eduardo Sáenz de Cabezón - Ganadores/as sorteo: @DanMaG175, @fUIHq39Ym40q4Ul - Más sobre estos libros en https://amzn.to/3F9x16w"))
+
